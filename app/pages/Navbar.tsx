c2ad7c7280800b2e6900bfd74b8109f248a6ea1f
Aman <aman.ver9345@gmail.com>
2026-07-18 14:24:08 +0530

    removed the y scales

--- app/pages/Navbar.tsx
+++ app/pages/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import ScaleX from "@/components/ScaleX";
-import ScaleY from "@/components/ScaleY";
 import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
@@ -16,33 +15,29 @@ export default function Navbar({ className }: { className?: string }) {
   }, []);
 
   return (
-    <div className="bg-background sticky top-0 z-50 w-full max-w-3xl">
-      <div className="grid w-full grid-cols-[16px_minmax(0,1fr)_16px]">
-        <ScaleY className="h-14 min-h-0" />
-        <div
-          className={cn(
-            "flex h-14 w-full items-center justify-between",
-            className,
-          )}
-        >
-          <div>
-            <h1 className="p-4 text-sm font-bold tracking-tight">$whoami</h1>
-          </div>
-          <div className="flex items-center justify-center">
-            <div className="p-4 text-sm font-bold tracking-tight">Projects</div>
-            <div className="p-4 text-sm font-bold tracking-tight">Blogs</div>
-            <div className="p-4 text-sm font-bold tracking-tight">Resume</div>
-            <div className="flex items-center justify-center p-4">
-              {mounted && (
-                <AnimatedThemeToggler
-                  theme={resolvedTheme as "light" | "dark"}
-                  onThemeChange={setTheme}
-                />
-              )}
-            </div>
+    <div className="bg-background sticky top-0 z-50 w-full max-w-3xl border-x">
+      <div
+        className={cn(
+          "flex h-14 w-full items-center justify-between px-4",
+          className,
+        )}
+      >
+        <div>
+          <h1 className="p-4 text-sm font-bold tracking-tight">$whoami</h1>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="p-4 text-sm font-bold tracking-tight">Projects</div>
+          <div className="p-4 text-sm font-bold tracking-tight">Blogs</div>
+          <div className="p-4 text-sm font-bold tracking-tight">Resume</div>
+          <div className="flex items-center justify-center p-4">
+            {mounted && (
+              <AnimatedThemeToggler
+                theme={resolvedTheme as "light" | "dark"}
+                onThemeChange={setTheme}
+              />
+            )}
           </div>
         </div>
-        <ScaleY className="h-14 min-h-0" />
       </div>
       <ScaleX />
     </div>
