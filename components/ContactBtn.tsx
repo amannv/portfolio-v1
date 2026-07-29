071233aaa14777bec0d64728f22354bcd8fe8f61
Aman <aman.ver9345@gmail.com>
2026-07-29 15:05:27 +0530

    deploy

--- components/ContactBtn.tsx
+++ components/ContactBtn.tsx
@@ -14,7 +14,7 @@ export default function ContactBtn({
       <div className="border-x p-1 sm:p-2">
         <a href={url} target="_blank" rel="noopener noreferrer">
           <Button
-            className={"px-1.5 py-3 shadow-none sm:px-3 sm:py-5"}
+            className={"px-1.5 py-5 shadow-none sm:px-3 sm:py-5"}
             variant={"outline"}
           >
             {icon} <span className="hidden sm:inline">{text}</span>
